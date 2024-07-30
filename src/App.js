f0ad2572037857ea24598ef96b73c6c33db20b27
munezero olivier hugue <munezeroolivierhuguesavator@gmail.com>
2024-07-30 08:30:39 +0200

    adding tailwind css

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <div className="font-poppins text-white bg-blue-950">
+      <div className="">
+          Hi am a software developer
+      </div>
+        <p className='text-blue-400'>
+          Edit and save to reload.
+        </p>
+          Learn React
+    </div>
+  );
+}
+
+export default App;
